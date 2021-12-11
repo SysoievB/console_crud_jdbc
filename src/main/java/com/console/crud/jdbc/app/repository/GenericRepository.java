3b35d4a2a3581dd165a5fd4c3998f8a559a66de1
SysoievB <sysoiev.bogdan.ua@gmail.com>
2021-12-11 05:42:34 +0100

    added interface of repo

---
+++ src/main/java/com/console/crud/jdbc/app/repository/GenericRepository.java
@@ -0,0 +1,15 @@
+package com.console.crud.jdbc.app.repository;
+
+import java.util.ArrayList;
+
+public interface GenericRepository<T, ID> {
+    T save(T data);
+
+    ArrayList<T> getAll();
+
+    T getById(ID id);
+
+    void update(ID id, T data);
+
+    void deleteById(ID id);
+}
